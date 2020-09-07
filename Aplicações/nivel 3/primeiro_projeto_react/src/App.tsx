0d5b92d4cb31a089421a64a9818a94051119b1c7
Matheus <matheus.ramos.silva.santos@gmail.com>
2020-09-07 04:02:38 -0300

    estilos gloabais finalizados

--- Aplicações/nivel 3/primeiro_projeto_react/src/App.tsx
+++ Aplicações/nivel 3/primeiro_projeto_react/src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 }
 
